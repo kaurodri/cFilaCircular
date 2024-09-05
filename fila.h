d17042ed0d44b3a1f7a4abdf086dc590382f20f8
HieA1lpha <engkauanrodrigues@gmail.com>
2024-09-05 19:01:13 -0300

    2024-09-05

---
+++ fila.h
@@ -0,0 +1,6 @@
+//Circular Queue
+//variação da linear, onde o último elemento está conectado de volta ao primeiro, formando um circulo
+//isso resolve o problema de desperdício de espaço na fila linear
+//
+//Entrega: 05.09.2024
+
